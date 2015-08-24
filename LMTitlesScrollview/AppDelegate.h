d1d140a6db9451ade87dbeeaa12ee726d76a6935
Ming Lu <583383489@qq.com>
2015-08-24 11:04:57 +0800

    Initial Commit

---
+++ LMTitlesScrollview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMTitlesScrollview
+//
+//  Created by mac on 15/8/24.
+//  Copyright (c) 2015年 migi Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LMTitlesScrollview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LMTitlesScrollview
+//
+//  Created by mac on 15/8/24.
+//  Copyright (c) 2015年 migi Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
